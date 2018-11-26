82d571e8e4b084c927ee1cc61a332f8081e04b29
Nicolas Mahecha <nicolasmelectronic@gmail.com>
2018-11-26 16:33:17 -0500

    logic update

--- functions.js
+++ functions.js
@@ -1,30 +1,99 @@
 $(document).ready(function() {
   $('.pregunta11').load('pregunta1.html',function () {
-    loadScripts();
+    $('.pregunta12').load('pregunta2.html',function () {
+      loadScripts();
+    });
   });
-  $('.pregunta12').load('pregunta2.html',function () {
-    loadScripts();
-  });
+
 })
 
 function loadScripts() {
-  var arraypreg = [0,0,0,0,0,0,0,0,0]
+  var arraypreg = [0,0,0,0]
   var arrayx = [0,0,0,0,0];
   //INICIO
   inicio();
+  crear_preguntas();
 
   function inicio() {
+    // nav de preguntass
+    $('.nav_preguntas').hide();
+    //esconde preguntas transversales
     $('#p_transversales').hide();
-    $('.preguntas_trans').hide();
-    $('.sig1').hide();
+
     $('.empezar').click(function() {
         $('.Instrucciones').hide(function() {
-          $('.preguntas_trans').show();
-          $('#p_transversales').show();
-          $('.pregunta2').hide();
+          empezar_diagnostico();
         }
       );
     })
+  }
+
+  function crear_preguntas() {
+    var preg = 'pregunta';
+    var cp = 0;
+    for (var i = 0; i < arraypreg.length; i++) {
+      cp++;
+      preg = preg + cp;
+      crear_respuestas(preg,cp);
+      preg = 'pregunta';
+    }
+  }
+
+  function crear_respuestas(pregunta,contpreg) {
+    var p = '#p';
+    for (var i = 1; i <= 5; i++) {
+      p=p+contpreg+i;
+      console.log(p);
+      $(p).click(function() {
+        // $(this).css('background','azure');
+        for(var j = 0;j < arrayx.length; j++) {
+          if ($(this).index() == j){
+            arrayx[j] = 1;
+            $(this).css('background','azure');
+          }
+        }
+        esconder(arrayx,contpreg);
+        for(var j = 0;j < arrayx.length; j++) {
+          arrayx[j] = 0;
+        }
+      })
+      p = '#p';
+    }
+  }
+
+  function empezar_diagnostico() {
+    var a = 1;
+    var pre = '.pregunta';
+    $('.nav_preguntas').show();
+    esconder_pregunta(1);
+  }
+
+  function esconder(r,cp) {
+    // console.log(p);
+    var blankCheckbox = '#blankCheckbox';
+    var p = '#p';
+    var a = 0;
+    for (var i = 0; i < r.length; i++) {
+      a++;
+      blankCheckbox = blankCheckbox + cp + a;
+      p = p + cp  + a;
+      if (r[i] == 0) {
+        $(blankCheckbox).hide();
+        $(p).show().css('background','transparent');
+      }
+      else {
+        $(blankCheckbox).show().css('background','azure');
+      }
+      // console.log(blankCheckbox+''+r[i]+''+p);
+      blankCheckbox = '#blankCheckbox';
+      p = '#p';
+    }
+  }
+
+  function esconder_pregunta(num_pregunta) {
+
+    var preg = '.pregunta' + num_pregunta;
+    $(preg).show();
   }
 
   //FUNCION CLICK
@@ -45,40 +114,4 @@ function loadScripts() {
   });
 
 
-  for (var i = 1; i <= 5; i++) {
-    var cont =0;
-    var p = '#p1';
-    p=p+i;
-    $(p).click(function() {
-      $(this).css('background','azure');
-      for(var j = 0;j < arrayx.length; j++) {
-        if ($(this).index() == j) arrayx[j] = 1;
-      }
-      esconder(arrayx);
-      for(var j = 0;j < arrayx.length; j++) {
-        arrayx[j] = 0;
-      }
-    })
-  }
-
-  function esconder(r) {
-    var blankCheckbox = '#blankCheckbox1';
-    var p = '#p1';
-    var a = 1;
-    for (var i = 0; i < r.length; i++) {
-      blankCheckbox = blankCheckbox + a;
-      p = p + a;
-      a++;
-      if (r[i] == 0) {
-        $(blankCheckbox).hide();
-        $(p).css('background',"transparent");
-      }
-      else {
-        $(blankCheckbox).show().css('background','azure');
-      }
-      console.log(blankCheckbox+''+r[i]+''+p);
-      blankCheckbox = '#blankCheckbox1';
-      p = '#p1';
-    }
-  }
 }
